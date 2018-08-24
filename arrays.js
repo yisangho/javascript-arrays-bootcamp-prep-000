53791f595774ccb602fc512b6e9cb836567ef525
yisangho <yisangho717@gmail.com>
2018-08-24 05:02:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -18,5 +18,9 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 
 function addElementToBeginningOfArray(array, element) {
+
+}
+
+function addElementToBeginningOfArray(array, 'foo') {
   
 }

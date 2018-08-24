d3826b5fb38b24ca694a1869a21c2c8eb662d4e9
yisangho <yisangho717@gmail.com>
2018-08-24 04:20:35 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var grilledCheeseIngredients = [
+  'bread',
+  'mild cheese',
+  'sharp cheese',
+  'butter',
+  'tomato',
+  'garlic'
+]
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
+
+var chocolateBars = [
+  'snickers', 'hundred grand', 'kitkat', 'skittles'
+]
+
+addElementToBeginningOfArray(chocolateBars, 'test');

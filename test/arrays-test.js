da98b880a85a821b610a4bf19347555cdad6a7a6
yisangho <yisangho717@gmail.com>
2018-08-24 04:55:54 +0000

    automatically backed up by learn

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -12,7 +12,7 @@ describe('arrays', () => {
 
   describe('chocolateBars', () => {
     console.log("===================")
-    //console.log(chocolateBars[0]);
+    console.log(chocolateBars[0]);
     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
       expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
     })

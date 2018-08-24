0515e09cb790fd02e7b955bfbd4567e5b4d3ed8d
yisangho <yisangho717@gmail.com>
2018-08-24 05:15:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,3 +25,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array
 }
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]
+}

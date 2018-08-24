3ca8c1198354c5b8eb885ee21bad622a13d8da13
yisangho <yisangho717@gmail.com>
2018-08-24 04:49:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,13 +4,16 @@ it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"
   expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
 })
 */
+
+/*
 var chocolateBars = new Array();
 
 chocolateBars.push("snickers");
 chocolateBars.push("hundred grand");
 chocolateBars.push("kitkat");
 chocolateBars.push("skittles");
-
+*/
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 console.log(chocolateBars[0]);
 
